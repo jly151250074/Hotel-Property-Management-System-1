97f3a1d8dd6f9a1c0baab5c9eda0736df21ca76b
Li Aobang <774164644@qq.com>
2016-10-16 23:07:40 +0800

    合并DatabaseFactory的更新

--- codes/stub/DataBaseFactory_Stub.java
+++ codes/stub/DataBaseFactory_Stub.java
@@ -1,18 +1,28 @@
-﻿public class DatabaseFactory_Stub implements DatabaseFactory {
+public class DatabaseFactory_Stub implements DatabaseFactory {
       //抽象工厂
-    public DataBaseFactory getUserInfoDatabase() {
+    public DataBaseFactory getUserInfoData() {
         UserInfoDatabaseService userInfoData = new UserInfoDataServiceMySqlImpl_Stub();
         return userInfoData;
     }
 
-    public DatabaseService getAccountDatabase(){
-	      AccountDatabase accountData = new AccountDatabaseServiceMySqlImpl_Stub();
-	      return accountData;
+    public DatabaseService getAccountData() {
+	 AccountDatabase accountData = new AccountDatabaseServiceMySqlImpl_Stub();
+	 return accountData;
     }
 
-    public DatabaseService getHotelDatabase(){
-	      HotelDataService hotelData = new HotelDataServiceMysqlImpl_Stub();
-	      return hotelData;
+    public DatabaseService getHotelData() {
+	 HotelDataService hotelData = new HotelDataServiceMysqlImpl_Stub();
+	 return hotelData;
+    }
+
+    public DatabaseService getOrderData(){
+	 OrderDataService orderData = new OrderDataServiceMysqlImpl_Stub();
+	 return orderData;
+    }
+
+    public DatabaseService getPromotionData(){
+	 PromotionDataService promotionData = new PromotionDataServiceMysqlImpl_Stub();
+	 return promotionData;
     }
 
 }
