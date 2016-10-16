a0b731298939b38702e5c6d3b3e0e07849fe96ec
Molloh <mollohjoe@gmail.com>
2016-10-16 23:22:13 +0800

    司马client

--- codes/main/Client.java
+++ codes/main/Client.java
@@ -4,24 +4,21 @@ public class Client {
         UserInfoBLService_Driver userBLService_driver = new UserInfoBLService_Driver();
         userBLService_driver.drive(userInfoController);
 
-
-    AccountBLService accontController = new AccountController();
+        AccountBLService accontController = new AccountController();
         AccountBLService_Driver accountBLService_driver = new AccountBLService_Driver();
-		accountBLService_driver.drive(accontController);
+		    accountBLService_driver.drive(accontController);
 
-
-    OrderBLService orderController = new OrderController();
+        OrderBLService orderController = new OrderController();
         OrderBLService_Driver orderBLService_driver = new OrderBLService_Driver();
-		orderBLService_driver.drive(orderController);
-    }
+		    orderBLService_driver.drive(orderController);
 
-    PromotionBLService promotionController = new PromotionController();
+        PromotionBLService promotionController = new PromotionController();
         PromotionBLService_Driver promotionBLService_driver = new PromotionBLService_Driver();
-		promotionBLService_driver.drive(promotionController);
-    }
+		    promotionBLService_driver.drive(promotionController);
 
-    HotelBLservice hotelController = new hotelController();
-    HotelBLservice_Driver hotelBLservice_Driver = new HotelBLservice_Driver();
-    hotelBLservice_Driver.drive(hotelController);
+        HotelBLservice hotelController = new hotelController();
+        HotelBLservice_Driver hotelBLservice_Driver = new HotelBLservice_Driver();
+        hotelBLservice_Driver.drive(hotelController);
+    }
 
 }
