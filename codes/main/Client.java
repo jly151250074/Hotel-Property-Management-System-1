dfd0785a8bd1c72bafb39073751fe18173158dd0
Li Aobang <774164644@qq.com>
2016-10-16 23:02:32 +0800

    重新上传Client.java

---
+++ codes/main/Client.java
@@ -0,0 +1,27 @@
+public class Client {
+    public static void main(String[] args) {
+        UserInfoBLService  userInfoBLServiceController = new UserInfoController();
+        UserInfoBLService_Driver userBLService_driver = new UserInfoBLService_Driver();
+        userBLService_driver.drive(userInfoController);
+
+
+    AccountBLService accontController = new AccountController();
+        AccountBLService_Driver accountBLService_driver = new AccountBLService_Driver();
+		accountBLService_driver.drive(accontController);
+
+
+    OrderBLService orderController = new OrderController();
+        OrderBLService_Driver orderBLService_driver = new OrderBLService_Driver();
+		orderBLService_driver.drive(orderController);
+    }
+
+    PromotionBLService promotionController = new PromotionController();
+        PromotionBLService_Driver promotionBLService_driver = new PromotionBLService_Driver();
+		promotionBLService_driver.drive(promotionController);
+    }
+
+    HotelBLservice hotelController = new hotelController();
+    HotelBLservice_Driver hotelBLservice_Driver = new HotelBLservice_Driver();
+    hotelBLservice_Driver.drive(hotelController);
+
+}
