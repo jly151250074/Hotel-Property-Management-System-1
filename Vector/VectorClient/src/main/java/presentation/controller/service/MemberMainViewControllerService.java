fe283d33a527f1ec80176268ee58de15fd80d6de
chinalienming <728385437@qq.com>
2016-11-27 21:04:52 +0800

    Update MemberMainViewControllerService.java

--- Vector/VectorClient/src/main/java/presentation/controller/service/MemberMainViewControllerService.java
+++ Vector/VectorClient/src/main/java/presentation/controller/service/MemberMainViewControllerService.java
@@ -1,9 +1,21 @@
-package presentation.controller.service;
+package presentation.controller;
+
+import common.ResultMessage;
+import vo.MemberVo;
 
 /**
- * @ author Molloh
- * @ version 2016/11/6
+ * @ author lienming
+ * @ version 2016-11-27
  * @ description
  */
 public interface MemberMainViewControllerService {
+	
+
+    public int getCredit(String id) ;
+
+    public ResultMessage chargeCredit(String id, int amount) ;
+
+    public MemberVo getInfo(String id) ;
+
+    public ResultMessage modifyInfo(MemberVo vo) ;
 }
