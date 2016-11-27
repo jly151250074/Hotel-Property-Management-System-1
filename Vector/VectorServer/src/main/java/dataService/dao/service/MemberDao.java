c6d0212979ff2a8df7f8cabd3c94ec1ae15c2ddf
chinalienming <728385437@qq.com>
2016-11-27 21:18:12 +0800

    Update MemberDao.java

--- Vector/VectorServer/src/main/java/dataService/dao/service/MemberDao.java
+++ Vector/VectorServer/src/main/java/dataService/dao/service/MemberDao.java
@@ -1,26 +1,29 @@
-package dataService.dao.service;
+package dataService.dao;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import common.ResultMessage;
-import po.MemberPo;
+import vo.MemberVo;
 
 /**
  * @ author lienming
- * @ version 2016/11/20
+ * @ version 2016-11-27
  * @ description
  */
 
 /*
 * 注意： id : int 全自行改为 String 类型
 */
-public interface MemberDao {
+public interface MemberDao extends Remote{
 
-    public int getCredit(int id);
+    public int getCredit(String id)throws RemoteException  ;
 
-    public ResultMessage chargeCredit(int id, int amount);
+    public ResultMessage chargeCredit(String id, int amount)throws RemoteException ;
 
-    public MemberPo getInfo(int id);
+    public MemberVo getInfo(String id)throws RemoteException ;
 
-    public ResultMessage modifyInfo(int id, MemberPo vo);
+    public ResultMessage modifyInfo( MemberVo vo)throws RemoteException ;
 
 
 }
