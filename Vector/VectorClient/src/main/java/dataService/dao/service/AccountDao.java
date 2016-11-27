b12e1dbc00d463bb3424dace3735295d4b74bbce
chinalienming <728385437@qq.com>
2016-11-27 20:58:15 +0800

    Update AccountDao.java

--- Vector/VectorClient/src/main/java/dataService/dao/service/AccountDao.java
+++ Vector/VectorClient/src/main/java/dataService/dao/service/AccountDao.java
@@ -1,33 +1,33 @@
-package dataService.dao.service;
+package dataService.dao;
+
+import java.rmi.Remote;
 
 import common.AccountType;
 import common.ResultMessage;
 import po.AccountPo;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import vo.AccountVo;
 
 /**
  * @ author lienming
- * @ version 2016/11/20
+ * @ version 2016/11/27
  * @ description
  */
 public interface AccountDao extends Remote {
 
-    public AccountType login(String id, String password) throws RemoteException;
+    public AccountType login(String id, String password);
 
-    public ResultMessage logout(String id) throws RemoteException ;
+    public ResultMessage logout(String id)  ;
 
-    public String register(String memberName,String password) throws RemoteException;
+    public String register(String memberName,String password) ;
 
-    public ResultMessage modify(String id,String newPassword) throws RemoteException;
+    public ResultMessage modify(String id,String newPassword) ;
 
-    public ResultMessage find(String id) throws RemoteException;
+    public AccountVo find(String id);
 
-    public ResultMessage insert(AccountPo po) throws RemoteException;
+    public ResultMessage insert(AccountPo po) ;
 
-    public ResultMessage update(AccountPo po) throws RemoteException;
+    public ResultMessage update(AccountPo po);
 
-    public ResultMessage delete(AccountPo po) throws RemoteException;
+    public ResultMessage delete(String id);
 
 }
