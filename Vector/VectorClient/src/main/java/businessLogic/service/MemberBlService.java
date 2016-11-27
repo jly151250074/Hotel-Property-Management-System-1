78ad729195ce45d11520a3aecbacd6ec99263b27
chinalienming <728385437@qq.com>
2016-11-27 20:53:50 +0800

    Update MemberBlService.java

--- Vector/VectorClient/src/main/java/businessLogic/service/MemberBlService.java
+++ Vector/VectorClient/src/main/java/businessLogic/service/MemberBlService.java
@@ -1,22 +1,17 @@
-package businessLogic.service;
+package businesslogic;
 
-import common.ResultMessage;
+
 import common.InfoType;
+import common.ResultMessage;
 import vo.MemberVo;
 
-/**
- * UserBLService
- * 负责实现用户信息界面所需要的服务
- * @author lienming
- * @version Nov 13, 2016
- */
 /**
  * 会员的属性有：
  * 信用值、VIP等级
  * 会员基本信息 - 用户名、联系方式、生日
  * 权限（会员）
  * @author lienming
- * @version Nov 13, 2016
+ * @version 2016-11-17
  */
 
 public interface MemberBlService {
@@ -24,29 +19,49 @@ public interface MemberBlService {
 	/**
 	 * 获取用户的信用值
 	 * @param id
-	 * @return 处理信息
+	 * @return credit:int
 	 * @author lienming
-	 * @version Nov 13, 2016
+	 * @version 2016-11-27
 	 */
-	public int getCredit(String id);
+	 public int getCredit(String id) ;
 
 	/**
 	 * 为客户充值信用
-	 * @param id 接受充值的会员账号, quantity 充值的信用数量
-	 * @return 处理信息
-	 * @author Molloh
-	 * @version Oct 14, 2016
+	 * @param id :int, amount :int充值的信用数量 可为负数
+	 * @return ResultMessage
+	 * @author lienming
+	 * @version 2016-11-27
 	 */
-	public ResultMessage chargeCredit(String id, int amount);
+	public ResultMessage chargeCredit(String id, int amount) ;
 
 	/**
 	 * 显示用户的信息
 	 * @param id
-	 * @return 用户信息
-	 * @author Molloh
-	 * @version Oct 14, 2016
+	 * @return MemberVo
+	 * @author lienming
+	 * @version 2016-11-27
 	 */
-	public MemberVo getInfo(String id);
+	public MemberVo getInfo(String id) ;
+
+	/**
+	 * 检查输入信息的合法性
+	 * @param info:String
+	 * @return ResultMessage
+	 * @author lienming
+	 * @version 2016-11-27
+	 */
+	public ResultMessage checkInfo(String info,InfoType infoType) ;
+
+	/**
+	 * 修改用户信息
+	 * @param vo 
+	 * @return ResultMessage
+	 * @author lienming
+	 * @version 2016-11-27
+	 */
+	public ResultMessage modifyInfo(MemberVo vo);
+}
+
 
 	/**
 	 * 检查输入信息的合法性
