8883e43429ed6476014564c6744e5528d10264ce
chinalienming <728385437@qq.com>
2016-11-27 21:20:13 +0800

    Update MemberDaoImpl.java

--- Vector/VectorServer/src/main/java/dataService/dao/Impl/MemberDaoImpl.java
+++ Vector/VectorServer/src/main/java/dataService/dao/Impl/MemberDaoImpl.java
@@ -1,27 +1,24 @@
-package dataService.dao.impl;
-
-
-import common.ResultMessage;
-import dataService.dao.service.MemberDao;
-import dataService.dataHelper.service.DataFactory;
-import dataService.dataHelper.Impl.DataFactoryImpl;
-import dataService.dataHelper.service.MemberDataHelper;
-import po.MemberPo;
+package dataService.dao.Impl;
 
 
 import java.util.Iterator;
 import java.util.Map;
 
+import common.ResultMessage;
+import dataService.dao.MemberDao;
+import dataService.dataHelper.DataFactory;
+import dataService.dataHelper.MemberDataHelper;
+import dataService.dataHelper.Impl.DataFactoryImpl;
+import po.MemberPo;
+import vo.MemberVo;
+
 /**
- * Created by Administrator on 2016-11-13.
+ * Created by Administrator on 2016-11-27.
  */
 
-/*
-* 注意： id : int 全自行改为 String 类型
-*/
 public class MemberDaoImpl implements MemberDao {
 
-    private Map<Integer,MemberPo> map;
+    private Map<String,MemberPo> map;
 
     private MemberDataHelper memberDataHelper;
 
@@ -46,7 +43,91 @@ public class MemberDaoImpl implements MemberDao {
     }
 
 
-    public int getCredit(int id){
+    public int getCredit(String id) {
+    	MemberVo vo = getInfo(id);
+    	if(!vo.equals(null))
+    		return vo.getCredit();
+    	else
+    		return -1;
+    }
+
+    public ResultMessage chargeCredit(String id, int amount){
+    	// amount<0?
+        Iterator<Map.Entry<String, MemberPo>> iterator = map.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<String,MemberPo> entry = iterator.next();
+            if( entry.getKey().equals(id) ) {
+                MemberPo po = entry.getValue();
+                po.setCredit(po.getCredit()+amount);
+                update(po);
+                return ResultMessage.SUCCEED;
+            }
+
+        }
+        return ResultMessage.FAIL;
+    }
+
+    public MemberVo getInfo(String id){
+        Iterator<Map.Entry<String, MemberPo>> iterator = map.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<String, MemberPo> entry = iterator.next();
+            if (entry.getKey().equals(id)) {
+                MemberPo po = entry.getValue();
+                MemberVo vo = new MemberVo(po);
+                return vo;
+            }
+        }
+        return null;
+    }
+
+    public ResultMessage modifyInfo(MemberVo vo){
+        Iterator<Map.Entry<String, MemberPo>> iterator = map.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<String, MemberPo> entry = iterator.next();
+            if ( entry.getKey().equals(vo.getId())) {
+            	MemberPo po = new MemberPo(vo.getId(),vo.getName(),vo.getPhone(),
+            			vo.getAddress(), vo.getSex(), vo.getCredit());
+                update(po);
+                return ResultMessage.SUCCEED;
+            }
+        }
+        return ResultMessage.FAIL;
+    }
+
+    public ResultMessage insert(MemberPo po){
+    	 if(!map.containsKey(po.getId())) {
+             map.put(po.getId(), po);
+             memberDataHelper.updateMemberData(map);
+             return ResultMessage.SUCCEED;
+         }
+         else
+             return ResultMessage.FAIL; //已存在
+    }
+    
+    public ResultMessage update(MemberPo po){
+    	String id = po.getId() ;
+        if(map.containsKey(id))
+        {
+            map.put(id, po);
+            memberDataHelper.updateMemberData(map);
+            return ResultMessage.SUCCEED;
+        }
+        else
+            return ResultMessage.FAIL;
+    }
+    
+    public ResultMessage delete(String id){
+    	  if(map.containsKey(id))
+          {
+              map.remove(id);
+              memberDataHelper.updateMemberData(map);
+              return ResultMessage.SUCCEED;
+          }
+          else
+              return ResultMessage.FAIL;
+    }
+    
+}
         return getInfo(id).getCredit();
     }
 
